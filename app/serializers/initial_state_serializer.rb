aa00e9b8b148a6fae2ee3e556ef6b24db23749cd
PigeonsHouse <bullbull1230@gmail.com>
2024-11-25 20:48:39 +0900

    add: C3公式サイト、ToyBoxへのリンクの追加
    Co-authored-by: Bokume2 <nagatomoyuu2@gmail.com>

--- app/serializers/initial_state_serializer.rb
+++ app/serializers/initial_state_serializer.rb
@@ -15,6 +15,10 @@ class InitialStateSerializer < ActiveModel::Serializer
   def meta
     store = default_meta_store
 
+    store[:c3_official_site_url] = ENV.fetch('C3_OFFICIAL_SITE_URL') if ENV['C3_OFFICIAL_SITE_URL'].present?
+
+    store[:c3_toybox_url] = ENV.fetch('C3_TOYBOX_URL') if ENV['C3_TOYBOX_URL'].present?
+
     if object.current_account
       store[:me]                = object.current_account.id.to_s
       store[:boost_modal]       = object_account_user.setting_boost_modal
